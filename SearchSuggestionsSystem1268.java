8b4ea8f98362ba4a7d0ed17fca0bc06558ba3f1c
Zhdanov Nikolai <nikolai@zhdanov.space>
2022-06-21 20:53:22 +0300

    improved 1268th problem

--- SearchSuggestionsSystem1268.java
+++ SearchSuggestionsSystem1268.java
@@ -1,70 +1,43 @@
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Arrays;
 
 public class SearchSuggestionsSystem1268 {
     public static void main (String[] args) {
-        String[] words = new String[] {"mobile","mouse","moneypot","monitor","mousepad"};
+        String[] words = new String[] {"mobile","mouse","monitors","moneypot","monitor","mousepad"};
         System.out.println(new Solution().suggestedProducts(words, "mouse"));
     }
 }
 
 class Solution {
-        public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-            Node root = new Node();
-            List<List<String>> res = new LinkedList<>();
-            for (int i = 0; i < products.length; i++) {
-                root.putString(products[i]);
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        Arrays.sort(products);
+        List<List<String>> res = new LinkedList();
+        for (int i = 1; i <= searchWord.length(); i++) { 
+            String subs = searchWord.substring(0, i);
+            int k = bs(products, subs, 0, products.length-1);
+            System.out.println(products[k]);
+            System.out.println(subs);
+            List<String> row = new LinkedList();
+            for (int j = k; j < k + 4 && j < products.length; j++){
+                if (products[j].indexOf(subs) == 0)
+                    row.add(products[j]);
             }
-            for (int i = 1; i <= searchWord.length(); i++) {
-                String s = searchWord.substring(0, i);
-                List<String> row = searchSuggestions(root, s);
-                res.add(row);
-            }
-            return res;
-    }
-
-    private List<String> searchSuggestions(Node r, String s) {
-        Node n = r;
-        for (int i = 0; i < s.length(); i++) {
-            Node next = n.getNode(s.charAt(i));
-            if (next == null) return new LinkedList();
-            r=n;
-            n = next;
+            res.add(row);
         }
-        List<String> res = new LinkedList();
-        if (r.end) res.add(s);
-        n.firstThree(s, res);
         return res;
     }
-}
 
-class Node {
-    private Node[] s = new Node[26];
-    public boolean end = false;
-
-
-    public void putString(String sin) {
-        if(sin.length() == 0) {
-            return;
+    private int bs(String[] p, String s, int l, int r) {
+        while (l+1 < r) {
+            int m = (l + r) / 2;
+            if (p[m].compareTo(s) > 0) {
+                r = m;
+            } else {
+                l = m;
+            }
         }
-        int first = sin.charAt(0) - 'a';
-        s[first] = s[first]==null?new Node():s[first];
-        s[first].end |= sin.length() == 1;
-        if (sin.length() > 1)
-            s[first].putString(sin.substring(1, sin.length()));
-    } 
-
-    public Node getNode(Character c) {
-        return s[c - 'a'];
-    }
-
-    public int firstThree(String ss, List<String> list) {
-        if (end) list.add(ss);
-        if (list.size()==3) return 3;
-        for (int i = 0; i < 26; i++) {
-            if (s[i] != null && s[i].firstThree(ss + (char)('a' + i), list) == 3) 
-                return 3;
-        }
-        return list.size();
+        return l;
     }
 }
+
